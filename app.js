21b185d2ac9a3674a0483a7466b87db97260c911
Ford Fowler <fordjfowler@gmail.com>
2016-09-20 14:57:36 -0700

    check if user answers are right and alert them

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 'use strict';
 
 var name = prompt('What\'s your name?');
-var answer1 = prompt('If I am a Leo, was I born in 1993?');
-var answer2 = prompt('Is my favorite Harry Potter creature the hippogriff?');
-var answer3 = prompt('Do I know to dance the Soulja BoY?');
-var answer4 = prompt('Are the inner depths of my soul pitched in total blackness?')
-var answer5 = prompt('Do I have any hope for the future of America?');
+
+var correctOrNot = 'correct';
+var answer1 = prompt('If I am a Leo, was I born in 1993?').toLowerCase();
+if (answer1 == 'yes' || answer1 == 'y') {
+  correctOrNot = 'incorrect';
+}
+alert(name + ', you are ' + correctOrNot + '!');
+
+correctOrNot = 'incorrect';
+var answer2 = prompt('Is my favorite Harry Potter creature the hippogriff?').toLowerCase();
+if (answer2 == 'yes' || answer2 == 'y') {
+  correctOrNot = 'correct';
+}
+alert(name + ', you are ' + correctOrNot + '!');
+
+correctOrNot = 'incorrect';
+var answer3 = prompt('Do I know to dance the Soulja BoY?').toLowerCase();
+if (answer3 == 'yes' || answer3 == 'y') {
+  correctOrNot = 'correct';
+}
+alert(name + ', you are ' + correctOrNot + '!');
+
+correctOrNot = 'correct';
+var answer4 = prompt('Are the inner depths of my soul pitched in total blackness?').toLowerCase();
+if (answer4 == 'yes' || answer4 == 'y') {
+  correctOrNot = 'incorrect';
+}
+alert(name + ', you are ' + correctOrNot + '!');
+
+correctOrNot = 'incorrect';
+var answer5 = prompt('Do I have any hope for the future of America?').toLowerCase();
+if (answer5 == 'yes' || answer5 == 'y') {
+  correctOrNot = 'correct';
+}
+alert(name + ', you are ' + correctOrNot + '!');
