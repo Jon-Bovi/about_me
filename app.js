663670c2996c812ba4467dea37a09f7ca9e3dbfc
Ford Fowler <fordjfowler@gmail.com>
2016-09-21 10:21:48 -0700

    refine js and css

--- app.js
+++ app.js
@@ -2,21 +2,22 @@
 
 var username = prompt('What\'s your username?');
 
+// Default correctness sets to incorrect before each question
 // Question 1
-var correctOrNot = 'correct';
+var correctOrNot = 'incorrect';
 var answer1 = prompt('If I am a Leo, was I born in 1993?').toLowerCase();
 console.log('User answer1 is ' + answer1);
-if (answer1 == 'yes' || answer1 == 'y') {
-  correctOrNot = 'incorrect';
+if (answer1 === 'no' || answer1 === 'n') {
+  correctOrNot = 'correct';
 }
 alert(username + ', you are ' + correctOrNot + '!');
 
 // Question 2
-correctOrNot = 'correct';
+correctOrNot = 'incorrect';
 var answer2 = prompt('Is my favorite Harry Potter creature the hippogriff?').toLowerCase();
 console.log('User answer2 is ' + answer2);
-if (answer2 == 'yes' || answer2 == 'y') {
-  correctOrNot = 'incorrect';
+if (answer2 === 'no' || answer2 === 'n') {
+  correctOrNot = 'correct';
 }
 alert(username + ', you are ' + correctOrNot + '!');
 
@@ -24,17 +25,17 @@ alert(username + ', you are ' + correctOrNot + '!');
 correctOrNot = 'incorrect';
 var answer3 = prompt('Do I know to dance the Soulja BoY?').toLowerCase();
 console.log('User answer3 is ' + answer3);
-if (answer3 == 'yes' || answer3 == 'y') {
+if (answer3 === 'yes' || answer3 === 'y') {
   correctOrNot = 'correct';
 }
 alert(username + ', you are ' + correctOrNot + '!');
 
 // Question 4
-correctOrNot = 'correct';
+correctOrNot = 'incorrect';
 var answer4 = prompt('Are the inner depths of my soul pitched in total blackness?').toLowerCase();
 console.log('User answer4 is ' + answer4);
-if (answer4 == 'yes' || answer4 == 'y') {
-  correctOrNot = 'incorrect';
+if (answer4 === 'no' || answer4 === 'n') {
+  correctOrNot = 'correct';
 }
 alert(username + ', you are ' + correctOrNot + '!');
 
@@ -42,7 +43,7 @@ alert(username + ', you are ' + correctOrNot + '!');
 correctOrNot = 'incorrect';
 var answer5 = prompt('Do I have any hope for the future of America?').toLowerCase();
 console.log('User answer5 is ' + answer5);
-if (answer5 == 'yes' || answer5 == 'y') {
+if (answer5 === 'yes' || answer5 === 'y') {
   correctOrNot = 'correct';
 }
 alert(username + ', you are ' + correctOrNot + '!');
